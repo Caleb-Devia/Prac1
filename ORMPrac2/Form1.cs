1b24b63dadfcfa14166079e23ed32af80977aba7
Caleb-Devia <cdevia1@udi.edu.co>
2021-10-14 21:46:47 -0500

    ORMPrac2

--- ORMPrac2/Form1.cs
+++ ORMPrac2/Form1.cs
@@ -16,5 +16,47 @@ namespace ORMPrac2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (Model.DB2EntityContainer db = new Model.DB2EntityContainer())
+            {
+
+                    using (var dbTransaction = db.Database.BeginTransaction())
+                    {
+                        try
+                        {
+                            List<Model.AGENTS> agents = new List<Model.AGENTS>();
+
+                            agents.Add(new Model.AGENTS { AGENT_CODE = 6, AGENT_NAME = "Ramasundar", WORKING_AREA = "Bangalore", COMMISSION = 0.15m, COUNTRY = "", PHONE_NO = "077-25814763" });
+                            agents.Add(new Model.AGENTS { AGENT_CODE = 3, AGENT_NAME = "Alex", WORKING_AREA = "London", COMMISSION = 0.13m, COUNTRY = "", PHONE_NO = "075-12458969" });
+
+                            db.AGENTS.AddRange(agents);
+                            db.SaveChanges();
+                            dbTransaction.Commit();
+
+                            MessageBox.Show("Database has been populated successfully", "Successful operation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        catch (Exception ex)
+                        {
+                            dbTransaction.Rollback();
+                            MessageBox.Show("An unexpected error occured, database could not be filled. \n\nApplication will be closed.", "Error found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            Application.Exit();
+
+                            return;
+                        }
+                    }
+
+                    MessageBox.Show("Database has been populated successfully", "Successful operation", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                button1.Enabled = false;
+            }
+        }
     }
 }
